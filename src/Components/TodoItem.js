f03be6ba1233f85efbf16e05dc1ad0cecd0207a6
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-10-05 12:32:23 -0400

    updated to fix console.log in handle delete

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -13,7 +13,7 @@ class TodoItem extends React.Component {
       <div className="row" id={this.props.id}> 
         {/* <input type="checkbox" checked={props.item.completed} onChange={props.item.completed}/>  */}
         <p>{this.props.item.text}</p>
-        <button onClick={() => this.props.onDelete}>X</button>
+        <button onClick={this.props.onDelete}>X</button>
       </div>
     )
   }
