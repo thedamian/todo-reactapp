e10563098b8684b2838398a79c0cb4495e5125b3
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-10-10 13:49:12 -0400

    updated code to try to see if item complete or not

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from "react";
 
 class TodoItem extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  render(){
-    return(
-      <div className="row" id={this.props.id}> 
-        {/* <input type="checkbox" checked={this.props.item.completed} onChange={this.props.onDelete.bind(this, this.props.item.id)}/>  */}
-        {/* <input type="checkbox" checked={this.props.item.completed} onChange={this.props.onDelete.bind(this, this.props.item)}/>  */}
-        {/* <input type="checkbox" checked={this.props.item.completed} onChange={this.item.completed = true}/>  */}
-        {/* <p style={this.props.item.completed ? {color: 'red'} : {color: 'white'}} onClick={this.props.item.completed = true}>{this.props.item.text}</p> */}
-        <p>{this.props.item.text}</p>
-        <button onClick={this.props.onDelete.bind(this, this.props.item.id)}>X</button>
+  render() {
+    let todoItem = this.props.item;
+
+    function strikeItem() {
+      todoItem.completed = !todoItem.completed;
+      console.log(todoItem.completed);
+    }
+
+    return (
+      <div className="row" id={this.props.id}>
+        <span
+          onClick={strikeItem}
+          style={{ color: todoItem.completed ? "green" : "red" }}
+        >
+          {todoItem.text}
+        </span>
+        <button onClick={this.props.onDelete.bind(this, this.props.item.id)}>
+          X
+        </button>
       </div>
-    )
+    );
   }
 }
 
-export default TodoItem
+export default TodoItem;
