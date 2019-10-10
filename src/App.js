48f3f36f79b15944187d9342da2ab54b0ee1dd36
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-10-10 12:26:20 -0400

    updated for loop to get the id of index

--- src/App.js
+++ src/App.js
@@ -1,167 +1,84 @@
-import React, {Component} from 'react'
-import './styles/App.css'
+import React, { Component } from "react";
+import "./styles/App.css";
 // import todosData from './Data/todosData'
-import TodoItem from './Components/TodoItem'
+import TodoItem from "./Components/TodoItem";
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       // data: todosData,
-      id: '',
+      id: "",
       todoArray: [],
-      inputText: ''
-      
-    }
-    this.handleBtnClick = this.handleBtnClick.bind(this)
-    this.getTextFromInput = this.getTextFromInput.bind(this)
-    this.handlesDelete = this.handlesDelete.bind(this)
-
+      inputText: ""
+    };
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.getTextFromInput = this.getTextFromInput.bind(this);
+    this.handlesDelete = this.handlesDelete.bind(this);
   }
 
-  render(){
- 
-  // console.log(this.state.data)
-  const todoItems = this.state.todoArray.map(item => <TodoItem key={item.id} item={item} onDelete={this.handlesDelete} />)
+  render() {
+    const todoItems = this.state.todoArray.map(item => (
+      <TodoItem key={item.id} item={item} onDelete={this.handlesDelete} />
+    ));
 
-    return(
-      <div className='App'>
+    return (
+      <div className="App">
         <h1> items </h1>
-        <span> 
-          {todoItems}
-        </span> 
-        <div className="row"> 
-          <input placeholder="test" type="text" id="input" onChange={this.getTextFromInput} />
-          <button onClick={this.handleBtnClick} >Add Item</button>
-        </div> 
+        <span>{todoItems}</span>
+        <div className="row">
+          <input
+            placeholder="test"
+            type="text"
+            id="input"
+            onChange={this.getTextFromInput}
+          />
+          <button onClick={this.handleBtnClick}>Add Item</button>
+        </div>
       </div>
-    )
+    );
   }
 
-     getTextFromInput(e) {
-       this.setState({
-         inputText: e.target.value
-       })
-     }
+  getTextFromInput(e) {
+    this.setState({
+      inputText: e.target.value
+    });
+  }
 
-    handleBtnClick() {
-      const todoItem = {
-        id: Date.now(),
-        text: this.state.inputText,
-        completed: false,
-        
+  handleBtnClick() {
+    const todoItem = {
+      id: Date.now(),
+      text: this.state.inputText,
+      completed: false
+    };
+
+    document.getElementById("input").value = "";
+
+    this.setState({
+      todoArray: [...this.state.todoArray, todoItem]
+    });
+  }
+
+  handlesDelete = id => {
+    // assign variable to state.todoArray
+    let defaultArray = this.state.todoArray;
+
+    // find the index of each item in the defaultArray
+    for (let i = 0; i < defaultArray.length; i++) {
+      let todoItem = defaultArray[i];
+
+      // check if the item in Default array equals to the passed ID
+      if (todoItem.id === id) {
+        // remove the item that equals that ID
+        defaultArray.splice(i, 1);
       }
-      console.log(todoItem)
-
-      document.getElementById('input').value = ''
-      
-      this.setState({
-        todoArray: [...this.state.todoArray, todoItem],
-      })
-      console.log(this.state.todoArray)
     }
 
-     handlesDelete = (id) => {
-       console.log(id)
-
-       
-
-       let holdArray = this.state.todoArray
-      //  let itemToRemove = holdArray.findIndex({id: id})
-
-      for (var i = 0; i < holdArray.length; i++){
-        var item = holdArray[i]
-        
-
-        if (item.indexOf(item.id !== id)){
-        } console.log(holdArray)
-      }
- 
-      
-      //  console.log(itemToRemove)
-      //  let holdArrayMinusRemovedItem = holdArray.splice(holdArray.indexOf(id), 1)
-      //  let holdArrayMinusRemovedItem = holdArray.filter(el => el !== id)
-       let holdArrayMinusRemovedItem = holdArray.splice(id, 1)
-      
-
-       console.log(holdArray)
-       console.log(holdArrayMinusRemovedItem)
-
-       this.setState({
-         todoArray: holdArrayMinusRemovedItem
-       })
-
-     }
-
-    
-
-   
+    // put in the new default array in side of todoArray in side of STATE
+    this.setState({
+      todoArray: defaultArray
+    });
+  };
 }
 
-
-
-
-
-/***
- * TEST CODE to UNDERSTAND STATE more
- */
-
-// class App extends Component {
-//   constructor(){
-//     super()
-//     this.state = {
-//       todoItems: [],
-//       todoLabel: ''
-//     }
-//     this.handleBtn = this.handleBtn.bind(this);
-//     this.handleInputText = this.handleInputText.bind(this)
-//   }
-
-  
- 
-//   render(){
-//     return (
-//       <div> 
-//         <input type="text" placeholder="enter todo here" onChange={this.handleInputText}/>
-//         <button onClick={this.handleBtn}> New todo</button>
-
-//         <TodoItem />
-//       </div> 
-//     )
-//   }
-
-//    handleInputText(e) {
-//      this.setState({
-//        todoLabel: e.target.value
-//      })
-//    }
-
-//    handleBtn() {
-//     console.log('btn clicked')
-
-//     //create new todoItem
-//     const newTodoItem = {
-//       name: this.state.todoLabel,
-//       id: Date.now()
-//     }
-
-//     this.setState({
-//       todoItems: [...this.state.todoItems, newTodoItem],
-//       todoLabel: ''
-//     })
-//     console.log(this.state.todoItems)
-//    }
-// }
-
-
-// class TodoItem extends Component {
-//   render(){
-//     return (
-//       <div> 
-//         <h2>this is todo item component</h2> 
-//       </div> 
-//     )
-//   }
-// }
-
-export default App
+export default App;
